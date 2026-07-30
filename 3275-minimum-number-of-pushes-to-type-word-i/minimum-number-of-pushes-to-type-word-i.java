8771c81b69344a1dfd0b63e9703037b9aff40825
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2026-07-30 18:04:10 +0530

    Time: 3 ms (11.24%) | Memory: 43.5 MB (16.51%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumPushes(String word) {
+       
+
+        int[] freq=new int[26];
+        for(char ch:word.toCharArray()){
+            freq[ch-'a']++;
+        }
+        Arrays.sort(freq);
+        int ans=0,index=0;
+        for(int i=25;i>=0;i--){
+            if(freq[i]==0){
+                break;
+            }
+            int pushes=index/8+1;
+            ans+=pushes*freq[i];
+            index++;
+        }
+        return ans;
+    }
+}
